9b49466824f4a57c6bbfd7227868d5b56c31524e
luisdelae <luisdelae@gmail.com>
2025-11-09 19:48:58 -0500

    Hamburger menu and remove unused file.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,38 +1,84 @@
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
-import { Android } from '@mui/icons-material';
+import { AppBar, Toolbar, Typography, Button, Box, IconButton, Drawer, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import { Android, Menu as MenuIcon } from '@mui/icons-material';
+import { useState } from 'react';
 
 function NavBar() {
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const handleScroll = (id) => {
+        document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
+        setDrawerOpen(false);
+    };
+
+    const handleDownload = () => {
+        window.open('https://github.com/luisdelae/hodler-android', '_blank');
+        setDrawerOpen(false);
+    };
+
+    const menuItems = [
+        { text: 'Features', action: () => handleScroll('features') },
+        { text: 'About', action: () => handleScroll('about') },
+        { text: 'Download', action: handleDownload }
+    ];
+
     return (
-        <AppBar position='static'>
-            <Toolbar>
-                <Android sx={{ mr: 2}} />
-                <Typography variant='h6' sx={{ flexGrow: 1}}>
-                    Hodler
-                </Typography>
-                <Box>
-                    <Button 
-                        color='inherit'
-                        onClick={() => document.getElementById('features').scrollIntoView({ behavior: 'smooth' })}
+        <>
+            <AppBar position='static'>
+                <Toolbar>
+                    <Android sx={{ mr: 2 }} />
+                    <Typography variant='h6' sx={{ flexGrow: 1 }}>
+                        Hodler
+                    </Typography>
+
+                    {/* Desktop buttons */}
+                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                        <Button color='inherit' onClick={() => handleScroll('features')}>
+                            Features
+                        </Button>
+                        <Button color='inherit' onClick={() => handleScroll('about')}>
+                            About
+                        </Button>
+                        <Button 
+                            color='inherit' 
+                            variant="outlined" 
+                            sx={{ ml: 1 }}
+                            onClick={handleDownload}
+                        >
+                            Download
+                        </Button>
+                    </Box>
+
+                    {/* Mobile menu icon */}
+                    <IconButton 
+                        color="inherit"
+                        edge="end"
+                        onClick={() => setDrawerOpen(true)}
+                        sx={{ display: { xs: 'block', sm: 'none' } }}
                     >
-                        Features
-                    </Button>
-                    <Button 
-                        color='inherit'
-                        onClick={() => document.getElementById('about').scrollIntoView({ behavior: 'smooth' })}
-                    >
-                        About
-                    </Button>
-                    <Button 
-                        color='inherit' 
-                        variant="outlined" 
-                        sx={{ ml: 1 }}
-                        onClick={() => window.open('https://github.com/luisdelae/hodler-android', '_blank')}
-                    >
-                        Download
-                    </Button>
+                        <MenuIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+
+            {/* Mobile Drawer */}
+            <Drawer
+                anchor="right"
+                open={drawerOpen}
+                onClose={() => setDrawerOpen(false)}
+            >
+                <Box sx={{ width: 250 }}>
+                    <List>
+                        {menuItems.map((item) => (
+                            <ListItem key={item.text} disablePadding>
+                                <ListItemButton onClick={item.action}>
+                                    <ListItemText primary={item.text} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
                 </Box>
-            </Toolbar>
-        </AppBar>
+            </Drawer>
+        </>
     );
 }
 
